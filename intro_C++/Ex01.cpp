72f57d05b2c04772dcb1a76d242f6a1629354faa
Piceratopo Finfina <buinhatminh2k52005@gmail.com>
2025-11-09 11:11:37 +0700

    add c++23 file

---
+++ intro_C++/Ex01.cpp
@@ -0,0 +1,9 @@
+// Ex1_01.cpp - A complete C++ program
+import std;
+// This line makes the entire Standard Library available,
+// including the std::println() functionality used below
+int main() {
+   int answer{42};  // Defines the variable answer with value 42
+   std::println("The answer to life, the universe, and everything is {}.", answer);
+   return 0;
+}
