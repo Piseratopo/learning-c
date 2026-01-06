afd116be2936b99396c0824f5df14b8d316f8073
buinhatminh2k52005 <buinhatminh2k52005@gmail.com>
2026-01-06 14:52:30 +0000

    the·ê

--- test generation_C++/create_test_case.py
+++ test generation_C++/create_test_case.py
@@ -1,23 +1,27 @@
 import random
 
-T = 100000
+T = 1000
 
 input_text = str(T) + "\n"
 output_text = ""
 
+for t in range(T):
+    # print(t)
+    x = random.randint(1, 1000)
+    y = random.randint(1, 1000)
+    c = random.randint(1, 1000)
+    input_text += f"{a} {b} {c}\n"
+    if a == b or c == -1:
+        output_text += "Vô nghiệm\n"
+        continue
+
+    x = (a - b) / (c + 1)
+    y = (a * c + b) / (c + 1)
+    z = (a - b) * c / (c + 1)
+    output_text += f"{round(x, 4)} {round(y, 4)} {round(z, 4)}\n"
+
 with open("input.txt", "w") as f:
     f.write(input_text)
+
 with open("output.txt", "w") as f:
     f.write(output_text)
-
-for t in range(T):
-    print(t)
-    a = ""
-    no_digits = 1000
-    for _ in range(no_digits):
-        a += str(random.randint(0, 9))
-    with open("input.txt", "a") as f:
-        f.write(str(a) + "\n")
-    with open("output.txt", "a") as f:
-        f.write("C" if int(a) % 11 == 0 else "K")
-        f.write("\n")
